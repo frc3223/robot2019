6d8090ae99b8506c8cd77e05c94465084dc6a0f0
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2019-01-12 08:27:19 -0800

    hello world!

---
+++ src/test/java/frc/robot/RobotTests.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RobotTests {
+
+    @Test
+    public void testTesting() {
+        assertEquals(1,1);
+    }
+}
