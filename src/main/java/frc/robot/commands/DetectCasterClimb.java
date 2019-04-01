10a919d0d273abc410e9300c9d4d79d8cd80242e
Madeline <woehrma0445m@ckstudents.org>
2019-04-01 12:08:25 -0700

    Merge branch 'master' of https://github.com/frc3223/robot2019
    # Conflicts:
    #	src/main/java/frc/robot/OI.java
    #	src/main/java/frc/robot/Robot.java
    #	src/main/java/frc/robot/RobotMap.java

---
+++ src/main/java/frc/robot/commands/DetectCasterClimb.java
@@ -0,0 +1,77 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DetectCasterClimb extends Command {
+  boolean climbedYet;
+  boolean rumbled;
+  Timer time;
+  boolean done;
+  public DetectCasterClimb() {
+    //requires(Robot.m_photoSensor);
+    time = new Timer();
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    climbedYet = false;
+    System.out.println("caster climb initalized");
+    rumbled = false;
+    done = false;
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if(Robot.m_oi.isCasterTarget()){
+      if(climbedYet){
+        Robot.m_oi.driverController.setRumble(Joystick.RumbleType.kLeftRumble,1.0);
+        time.start();
+        System.out.println("should rumble");
+        climbedYet = false;
+      }
+    }else{
+      //climbing
+      System.out.println("no longer sees ground");
+      climbedYet = true;
+    }
+    if(time.get() > 2.0 && !rumbled){
+      Robot.m_oi.driverController.setRumble(Joystick.RumbleType.kLeftRumble,0.0);
+      System.out.println("rumbling should stop");
+      rumbled = true;
+      time.stop();
+      done = true;
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return done;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
---
+++ src/main/java/frc/robot/commands/DetectFrontObject.java
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DetectFrontObject extends Command {
+  boolean rumbled;
+  Timer time;
+  public DetectFrontObject() {
+    //requires(Robot.m_photoSensor);
+    time = new Timer();
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    rumbled = false;
+    System.out.println("front object initalized");
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if(Robot.m_oi.isFrontTarget()){
+      if(!rumbled){
+        time.start();
+        System.out.println("should rumble");
+        Robot.m_oi.driverController.setRumble(Joystick.RumbleType.kRightRumble,1.0);
+        rumbled = true;  
+        }
+    }else{
+      rumbled = false;
+    }
+    if(time.get() > 2.0){
+      System.out.println("should stop rumbling");
+      Robot.m_oi.driverController.setRumble(Joystick.RumbleType.kRightRumble,0.0);
+      time.stop();
+      time.reset();
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
